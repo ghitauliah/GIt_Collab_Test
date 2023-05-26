881e27f476a0ebe009cce91b9542cc4f39ce70b2
petrusdrs <129033031+petrusdrs@users.noreply.github.com>
2023-05-26 10:40:16 +0700

    Initial Commit

---
+++ GIt_Collab_Test/GIt_Collab_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIt_Collab_TestApp.swift
+//  GIt_Collab_Test
+//
+//  Created by Petrus Sinaga on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GIt_Collab_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
